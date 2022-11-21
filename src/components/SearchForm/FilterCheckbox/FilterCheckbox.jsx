03d18cb86d511c3ed1e99fbbc2754ea7e012ca20
Oleg Dmitriev <92028362+Aper3d@users.noreply.github.com>
2022-11-21 11:25:09 +0500

    front 21.11.22 v3

--- src/components/SearchForm/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/SearchForm/FilterCheckbox/FilterCheckbox.jsx
@@ -1,11 +1,17 @@
 import './FilterCheckbox.css';
 
-function FilterCheckbox() {
+function FilterCheckbox({ checkbox, setCheckbox }) {
+
+  const onClickCheckBox = () => setCheckbox(!checkbox);
   return (
-    <label className='filter'>
-      <input className='filter__input' type='checkbox' />
-      <span className='filter__input_visible filter__input_visible_checked btn__hover' />
-      Короткометражки
+
+    <label className='filter' htmlFor='short-film'>
+      <input className='filter__input' type='checkbox' id='short-film'
+        value='short-film' checked={checkbox} onChange={onClickCheckBox} />
+      <div className="filter__pseudo-item btn__hover">
+        <span className="filter__circle"></span>
+      </div>
+      <span className='filter__titile'> Короткометражки</span>
     </label>
   )
 }
